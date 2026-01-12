b516d9e4d22a384b1dcec7685b8d4b7dfeb90893
hanul <ilkhanull@gmail.com>
2026-01-12 20:03:48 +0800

    perubahan title

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ const poppins = Poppins({
 })
 
 export const metadata = {
-  title: 'Portofolio Saya - Developer & Desainer',
+  title: 'Ilhanul - Graphic Designer',
   description: 'Portofolio profesional dengan showcase project dan skill',
 }
 
